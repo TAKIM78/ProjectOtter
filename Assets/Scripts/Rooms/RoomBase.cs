718fc19b5957164e5af0213113d6074724e45e21
Derebeyi <6437707+batuhanyus@users.noreply.github.com>
2022-05-09 23:58:56 +0300

    Sprint 1 sonu Plastic SCM'den GitHub'a proje aktarımı.

---
+++ Assets/Scripts/Rooms/RoomBase.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBase : MonoBehaviour
+{
+    public int posX, posY;
+    public int height, width;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
